9067f23c28f3deae182d51b3470f2ca23b0b2a87
Andrey Mishchenko <buryonok@gmail.com>
2020-05-03 18:09:58 +0300

    fixes

--- dist/bin/gendiff.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _cliBuilder = _interopRequireDefault(require("../cli-builder"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _cliBuilder.default)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vZ2VuZGlmZi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7O0FBRUE7Ozs7QUFFQSIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52IG5vZGVcblxuaW1wb3J0IGdlbmRpZmYgZnJvbSAnLi4vY2xpLWJ1aWxkZXInO1xuXG5nZW5kaWZmKCk7XG4iXX0=
