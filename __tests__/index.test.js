94dfd43efc456e4e109ff302a0c9eed67b6ba0f3
Andrey Mishchenko <buryonok@gmail.com>
2022-09-23 22:28:07 +0300

    Fix tests duplication

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,32 +4,24 @@ import genDiff from '../src/index';
 
 const pairsOfFilenames = [['before.json', 'after.json'], ['before.yml', 'after.yml'], ['before.ini', 'after.ini']];
 
-describe("'genDiff' utility", () => {
+const runTests = (resultFilename, outputFormat) => {
   test.each(pairsOfFilenames)(
     'given %p and %p',
     (before, after) => {
-      expect(genDiff(`fixtures/${before}`, `fixtures/${after}`))
-        .toBe(fs.readFileSync('fixtures/result', 'utf-8'));
+      expect(genDiff(`fixtures/${before}`, `fixtures/${after}`, outputFormat))
+        .toBe(fs.readFileSync(`fixtures/${resultFilename}`, 'utf-8'));
     },
   );
+};
+
+describe("'genDiff' utility", () => {
+  runTests('result');
 });
 
 describe("'genDiff plain' utility", () => {
-  test.each(pairsOfFilenames)(
-    'given %p and %p',
-    (before, after) => {
-      expect(genDiff(`fixtures/${before}`, `fixtures/${after}`, 'plain'))
-        .toBe(fs.readFileSync('fixtures/plain-result', 'utf-8'));
-    },
-  );
+  runTests('plain-result', 'plain');
 });
 
 describe("'genDiff JSON' utility", () => {
-  test.each(pairsOfFilenames)(
-    'given %p and %p',
-    (before, after) => {
-      expect(genDiff(`fixtures/${before}`, `fixtures/${after}`, 'json'))
-        .toBe(fs.readFileSync('fixtures/result.json', 'utf-8'));
-    },
-  );
+  runTests('result.json', 'json');
 });
