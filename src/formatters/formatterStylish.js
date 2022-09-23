83cb13189039dc8024ed68c5c598d33c404b3785
Andrey Mishchenko <buryonok@gmail.com>
2022-09-23 23:17:55 +0300

    Fix formatterStylish duplication

--- src/formatters/formatterStylish.js
+++ src/formatters/formatterStylish.js
@@ -4,25 +4,28 @@ const space = '  ';
 
 const iter = (diff, subDepth = 0) => diff.map((node) => {
   const depth = subDepth + 1;
+  const shortSpaceString = space.repeat(depth * 2 - 1);
+  const mediumSpaceString = space.repeat(depth * 2);
+  const longSpaceString = space.repeat(depth * 2 + 1);
   const stringify = (value) => {
     if (!_.isObject(value)) {
       return value;
     }
     const keys = Object.keys(value);
-    const result = keys.map((key) => `${space.repeat(depth * 2 + 1)}${key}: ${value[key]}`);
-    return `{\n  ${result.join('\n')}\n  ${space.repeat(depth * 2 - 1)}}`;
+    const result = keys.map((key) => `${longSpaceString}${key}: ${value[key]}`);
+    return `{\n  ${result.join('\n')}\n  ${shortSpaceString}}`;
   };
   switch (node.status) {
     case 'unchanged':
-      return `${space.repeat(depth * 2 - 1)}  ${node.name}: ${stringify(node.value)}`;
+      return `${shortSpaceString}  ${node.name}: ${stringify(node.value)}`;
     case 'added':
-      return `${space.repeat(depth * 2 - 1)}+ ${node.name}: ${stringify(node.value)}`;
+      return `${shortSpaceString}+ ${node.name}: ${stringify(node.value)}`;
     case 'deleted':
-      return `${space.repeat(depth * 2 - 1)}- ${node.name}: ${stringify(node.value)}`;
+      return `${shortSpaceString}- ${node.name}: ${stringify(node.value)}`;
     case 'changed':
-      return `${space.repeat(depth * 2 - 1)}+ ${node.name}: ${stringify(node.currentValue)}\n${space.repeat(depth * 2 - 1)}- ${node.name}: ${stringify(node.previousValue)}`;
+      return `${shortSpaceString}+ ${node.name}: ${stringify(node.currentValue)}\n${shortSpaceString}- ${node.name}: ${stringify(node.previousValue)}`;
     case 'nested':
-      return `${space.repeat(depth * 2)}${node.name}: {\n${iter(node.children, depth).join('\n')}\n${space.repeat(depth * 2)}}`;
+      return `${mediumSpaceString}${node.name}: {\n${iter(node.children, depth).join('\n')}\n${mediumSpaceString}}`;
     default:
       throw new Error(`Unknown node status: '${node.status}'!`);
   }
