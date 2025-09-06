288d9a2280311ae634eeaf083bb7dfa6301651d2
mishchenkoandrey <51906367+mishchenkoandrey@users.noreply.github.com>
2025-09-06 16:45:35 +0000

    Migrate to ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+// eslint.config.mjs
+import { globalIgnores } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  // Игнорируем сборки
+  globalIgnores(['**/dist']),
+
+  // Подключаем старые конфиги через compat
+  ...compat.extends(
+    'airbnb-base', // airbnb base
+    'plugin:jest/recommended', // jest recommended
+  ),
+
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+
+    rules: {
+      'no-console': 'off',
+      'import/extensions': 'off',
+      'jest/expect-expect': 'error',
+    },
+  },
+
+  {
+    files: ['eslint.config.mjs'],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-unresolved': 'off',
+    },
+  },
+];
